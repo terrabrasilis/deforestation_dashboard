b438b5894c2d2ed9fe2f3761aa26fdb6d53464a2
Paulo Luan <pauloluan.inova@gmail.com>
2019-09-16 14:21:54 -0300

    removed unused routes

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UserComponent } from './user/user.component';
-import { LoginComponent } from './login/login.component';
-import { MapComponent } from './map/map.component';
 import { WmsSearchComponent } from './wms/wms-search/wms-search.component';
 
 /*
@@ -12,10 +9,6 @@ import { DeforestationOptionsComponent } from './dashboard/deforestation/defores
 
 
 const routes: Routes = [
-    // { path : '', component : MapComponent },
-    { path: 'map/:type', component : MapComponent },
-    { path: 'user', component: UserComponent },
-    { path: 'login', component: LoginComponent },
     { path: 'wms', component: WmsSearchComponent },    
     { path: 'dashboard/deforestation/biomes/:biome/:type', component: DeforestationOptionsComponent },
     {
