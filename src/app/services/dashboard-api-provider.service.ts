569c944622cc9bff7b2f4a8f8051d4249b0942ce
André Carvalho <afacarvalho@yahoo.com.br>
2019-10-08 15:11:45 -0300

    Remove unused code

--- src/app/services/dashboard-api-provider.service.ts
+++ src/app/services/dashboard-api-provider.service.ts
@@ -1,9 +1,5 @@
 import { Injectable } from '@angular/core';
-
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-
-import { Observable } from 'rxjs/Observable';
-
+import { HttpClient } from '@angular/common/http';
 import { Constants } from '../util/constants';
 
 @Injectable() 
