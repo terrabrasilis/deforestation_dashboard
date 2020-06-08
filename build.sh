318238815534d2e2b80c3822b039e31553070638
André Carvalho <afacarvalho@yahoo.com.br>
2020-06-08 16:16:33 -0300

    Improve build script

--- build.sh
+++ build.sh
@@ -33,6 +33,7 @@ echo "...................................................."
 if [[ "$2" = "" ]]; then
     ENV="production"
     BUILD_TYPE="production"
+    VERSION="prod_$VERSION"
     read -p "I will build on production mode by default. May i continue? If yes, type yes or Ctrl+C to exit. " -d'y' -d'e' -d's' RESPONSE; echo
 else
 
@@ -42,6 +43,7 @@ else
     else
         ENV="production"
         BUILD_TYPE="production"
+        VERSION="prod_$VERSION"
     fi
 fi
 
@@ -49,7 +51,7 @@ echo "Building $BUILD_TYPE mode..."
 echo "........................"
 
 # --no-cache
-docker build --no-cache --build-arg ENV=$ENV --build-arg BUILD_TYPE=$BUILD_TYPE -t terrabrasilis/deforestation-dashboard:$VERSION -f Dockerfile .
+docker build --build-arg ENV=$ENV --build-arg BUILD_TYPE=$BUILD_TYPE -t terrabrasilis/deforestation-dashboard:$VERSION -f Dockerfile .
 
 echo "The building was finished! Do you want sending this new image to Docker HUB? Type yes to continue." ; read SEND_TO_HUB
 if [[ ! "$SEND_TO_HUB" = "yes" ]]; then
