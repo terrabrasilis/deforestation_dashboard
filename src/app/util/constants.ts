cccabe379e8b0b4f363af009c0dda0bfae8b0fdd
André Carvalho <afacarvalho@yahoo.com.br>
2021-05-20 11:41:24 -0300

    Fix localhost api url for development env and api on compose

--- src/app/util/constants.ts
+++ src/app/util/constants.ts
@@ -6,7 +6,7 @@ export class Constants {
     public static get DASHBOARD_API_HOST(): string {
         let url="http://terrabrasilis.dpi.inpe.br/dashboard/api/v1/redis-cli/";
 
-        if(process.env.BUILD_TYPE == 'development') url = "http://terrabrasilis.dpi.inpe.br/dashboard/api/v1/redis-cli/";
+        if(process.env.BUILD_TYPE == 'development') url = "http://localhost:3000/dashboard/api/v1/redis-cli/";
 
         if(process.env.BUILD_TYPE && process.env.ENV == 'production') {
             // confirm the 13111 port in docker-stacks/api/business-api-homologation.yaml
