755b3192b8003bf09616f99712a9f172af085b6d
andre-carvalho <afacarvalho@yahoo.com.br>
2022-11-17 16:01:35 -0300

    Change the rate file location.

--- src/app/util/constants.ts
+++ src/app/util/constants.ts
@@ -25,8 +25,9 @@ export class Constants {
     };
 
     public static get FILE_RATES(): string {
-        // return the name of JSON file with rates
-        return "assets/files/rates2022.json";
+        let url = (process.env.LOCAL_API == 'yes')?("assets/files/"):("http://terrabrasilis.dpi.inpe.br/download/dataset/legal-amz-prodes/json/");
+        // return the URL and name of JSON file with rates
+        return url+"rates2022.json";
     };
 
     public static get LAST_UPDATE_DATE(): string {
